66aabcbb90f7555ceb7034a447ea79c052332e7d
anegin <anegamega@gmail.com>
2023-05-18 17:38:24 +0300

    Lab1: C# add sample task code.

--- lab1/c_sharp/Program.cs
+++ lab1/c_sharp/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace c_sharp;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        new Program().Start();
+    }
+
+    private void Start()
+    {
+        new Thread(Calculator).Start();
+        new Thread(Calculator).Start();
+        new Thread(Calculator).Start();
+
+        Thread thread1 = new Thread(Calculator);
+        thread1.Start();
+
+        new Thread(Stopper).Start();
+    }
+
+    private void Calculator()
+    {
+        long sum = 0;
+        do
+        {
+            sum++;
+        } while (!CanStop);
+
+        Console.WriteLine(sum);
+    }
+
+    private bool CanStop { get; set; }
+
+    private void Stopper()
+    {
+        Thread.Sleep(3 * 1000);
+        CanStop = true;
+    }
+}
