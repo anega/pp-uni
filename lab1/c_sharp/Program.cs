7217e50a3e09c6ab4722e4b3c811a9632a6d99b5
anegin <anegamega@gmail.com>
2023-05-19 13:21:14 +0300

    Lab1: C# start threads in the loop; add thread id and sum step for Calculator.

--- lab1/c_sharp/Program.cs
+++ lab1/c_sharp/Program.cs
@@ -9,32 +9,34 @@ public class Program
 
     private void Start()
     {
-        new Thread(Calculator).Start();
-        new Thread(Calculator).Start();
-        new Thread(Calculator).Start();
-
-        Thread thread1 = new Thread(Calculator);
-        thread1.Start();
+        for (int i = 1; i <= 3; i++)
+        {
+            int threadId = i;
+            Thread myThread = new Thread(() => Calculator(threadId, 2));
+            myThread.Start();
+        }
 
         new Thread(Stopper).Start();
     }
 
-    private void Calculator()
+    private void Calculator(int threadId, int sumStep)
     {
         long sum = 0;
+        long elementsCount = 0;
         do
         {
-            sum++;
+            sum += sumStep;
+            elementsCount++;
         } while (!CanStop);
 
-        Console.WriteLine(sum);
+        Console.WriteLine($"Thread {threadId}: sum = {sum}, elements count: {elementsCount}");
     }
 
     private bool CanStop { get; set; }
 
     private void Stopper()
     {
-        Thread.Sleep(3 * 1000);
+        Thread.Sleep(30 * 1000);
         CanStop = true;
     }
 }
