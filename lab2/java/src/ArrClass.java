69ceeadf15effb5036481b8d08908943318b47b5
anegin <anegamega@gmail.com>
2023-05-23 22:10:32 +0300

    Lab2(java): add calculation of total min value and its index.

--- lab2/java/src/ArrClass.java
+++ lab2/java/src/ArrClass.java
@@ -1,13 +1,18 @@
+import java.util.HashMap;
+
 public class ArrClass {
     static final int SMALLEST_EL_INDEX = 3;
     private final int arrSize;
     private final int threadNum;
     private final int[] arr;
+    private final HashMap<String, Integer> totalMin = new HashMap<>();
 
     public ArrClass(int arrSize, int threadNum) {
         this.arrSize = arrSize;
         this.threadNum = threadNum;
         arr = new int[arrSize];
+        totalMin.put("minIndex", 0);
+        totalMin.put("minValue", 0);
 
         for (int i = 0; i < arrSize; i++) {
             if (i == SMALLEST_EL_INDEX) {
@@ -18,18 +23,30 @@ public class ArrClass {
         }
     }
 
-    public int getMinValue(int startIndex, int endIndex) {
-        int minValue = arr[startIndex];
+    synchronized public void setTotalMin(HashMap<String, Integer> min) {
+        int minValue = min.get("minValue");
+        int minIndex = min.get("minIndex");
+        if (minValue < totalMin.get("minValue")) {
+            totalMin.put("minIndex", minIndex);
+            totalMin.put("minValue", minValue);
+        }
+    }
+
+    public HashMap<String, Integer> getMinValue(int startIndex, int endIndex) {
+        HashMap<String, Integer> minElement = new HashMap<>();
+        minElement.put("minIndex", startIndex);
+        minElement.put("minValue", arr[startIndex]);
         for (int i = startIndex; i < endIndex; i++) {
-            if (minValue > arr[i]) {
-                minValue = arr[i];
+            if (minElement.get("minValue") > arr[i]) {
+                minElement.put("minIndex", i);
+                minElement.put("minValue", arr[i]);
             }
         }
 
-        return minValue;
+        return minElement;
     }
 
-    public void getTotalMinValue() {
+    public HashMap<String, Integer> getTotalMinValue() {
         ThreadMin[] threadMin = new ThreadMin[threadNum];
         int partSize = arrSize / threadNum;
 
@@ -42,5 +59,14 @@ public class ArrClass {
             threadMin[i] = new ThreadMin(startIndex, endIndex, this);
             threadMin[i].start();
         }
+        for (int i = 0; i < threadNum; i++) {
+            try {
+                threadMin[i].join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return totalMin;
     }
 }
