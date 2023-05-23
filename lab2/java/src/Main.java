b9f8a827a66f93cc87f153fc286f311230fa47e8
anegin <anegamega@gmail.com>
2023-05-23 22:13:00 +0300

    Lab2(java): add output of program results.

--- lab2/java/src/Main.java
+++ lab2/java/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
-    static final int ARR_SIZE = 20;
-    static final int THREAD_NUM = 3;
+    static final int ARR_SIZE = 100000000;
+    static final int THREAD_NUM = 8;
 
     public static void main(String[] args) {
         ArrClass arrClass = new ArrClass(ARR_SIZE, THREAD_NUM);
-        arrClass.getTotalMinValue();
+        int minIndex = arrClass.getTotalMinValue().get("minIndex");
+        int minValue = arrClass.getTotalMinValue().get("minValue");
+        System.out.println("Minimal array element is " + minValue + " and it's index is " + minIndex);
     }
 }
