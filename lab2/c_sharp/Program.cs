76d04b45173184577e234cc15e53e7adc70c19ec
anegin <anegamega@gmail.com>
2023-05-25 14:53:12 +0300

    Lab2(c#): generate an array with given params(size, smallest element).

--- lab2/c_sharp/Program.cs
+++ lab2/c_sharp/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Lab2;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    private const int ArrSize = 10000000;
+    private const int SmallestNum = -1;
+    private const int SmallestNumIndex = 3;
+    private readonly int[] _arr = new int[ArrSize];
+
+    private Program()
+    {
+        for (int i = 0; i < ArrSize; i++)
+        {
+            if (i == SmallestNumIndex)
+            {
+                _arr[i] = SmallestNum;
+                continue;
+            }
+
+            _arr[i] = i;
+        }
+    }
+
+    private static void Main()
+    {
+        Program program = new();
+    }
+}
