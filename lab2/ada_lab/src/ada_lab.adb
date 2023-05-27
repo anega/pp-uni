11f6e36169d6122cecfde5f1a9906cc084d1b04d
anegin <anegamega@gmail.com>
2023-05-27 17:06:47 +0300

    Lab2(ada): init an array with a given size and a smallest number.

--- lab2/ada_lab/src/ada_lab.adb
+++ lab2/ada_lab/src/ada_lab.adb
@@ -1,6 +1,26 @@
 with Ada.Text_IO;
 
 procedure Ada_Lab is
+   Arr_Size           : constant Integer := 9;
+   Smallest_Num       : constant Integer := -1;
+   Smallest_Num_Index : constant Integer := 4;
+
+   Arr : array (0 .. Arr_Size) of Integer;
+
+   procedure Init_Arr is
+   begin
+      for I in 0 .. Arr_Size loop
+         if I = Smallest_Num_Index then
+            Arr (I) := Smallest_Num;
+            goto Continue;
+         end if;
+         Arr (I) := I;
+         <<Continue>>
+      end loop;
+   end Init_Arr;
 begin
-   Ada.Text_IO.Put_Line ("Init");
+   Init_Arr;
+   for I in Arr'Range loop
+      Ada.Text_IO.Put (Arr (I)'Image & ",");
+   end loop;
 end Ada_Lab;
