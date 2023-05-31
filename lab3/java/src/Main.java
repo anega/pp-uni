cc7b5cab5dd191feece865648374f88f56ba171e
anegin <anegamega@gmail.com>
2023-05-31 21:34:31 +0300

    Lab3(java): define constants.

--- lab3/java/src/Main.java
+++ lab3/java/src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
+    public static final int BUFFER_SIZE = 3;
+    public static final int REQ_COUNT = 10;
+    public static final int PROD_COUNT = 4;
+    public static final int CONS_COUNT = 4;
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
