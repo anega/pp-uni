5517c31ee54c72451ef672bb1788b0a7d01ecba3
anegin <anegamega@gmail.com>
2023-06-06 14:48:12 +0300

    Lab3(c#): add semaphores and buffer variables.

--- lab3/c_sharp/c_sharp/Program.cs
+++ lab3/c_sharp/c_sharp/Program.cs
@@ -6,6 +6,10 @@ public class Program
     private const int ReqCount = 10;
     private const int ProdCount = 4;
     private const int ConsCount = 4;
+    private Semaphore _access = new Semaphore(1, 1);
+    private Semaphore _empty = new Semaphore(0, BufferSize);
+    private Semaphore _full = new Semaphore(BufferSize, BufferSize);
+    private readonly List<string> _buffer = new();
 
     private static void Main()
     {
