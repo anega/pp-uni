b83dad80051b2439a51dca6c40023bc48690cedd
anegin <anegamega@gmail.com>
2023-06-10 16:06:21 +0300

    Lab3(ada): store produced items in a list.

--- lab3/ada_lab/src/ada_lab.adb
+++ lab3/ada_lab/src/ada_lab.adb
@@ -1,13 +1,22 @@
-with Ada.Text_IO;
+with Ada.Text_IO;                                   use Ada.Text_IO;
+with Ada.Containers.Indefinite_Doubly_Linked_Lists; use Ada.Containers;
 
 procedure Ada_Lab is
+   package String_Lists is new Indefinite_Doubly_Linked_Lists (String);
+   use String_Lists;
+
+   Req_Count : constant Integer := 3;
+   Storage   : List;
 
    task type Producer;
    task type Consumer;
 
    task body Producer is
    begin
-      null;
+      for I in 1 .. Req_Count loop
+         Storage.Append ("Item " & I'Image);
+         Put_Line ("Item" & I'Image & " produced.");
+      end loop;
    end Producer;
 
    task body Consumer is
@@ -15,6 +24,8 @@ procedure Ada_Lab is
       null;
    end Consumer;
 
+   P : Producer;
+
 begin
-   Ada.Text_IO.Put_Line ("Init");
+   Put_Line ("Init");
 end Ada_Lab;
