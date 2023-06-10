3dc6447119b4626f4793e4cc543dd99b100f3ef8
anegin <anegamega@gmail.com>
2023-06-10 15:14:02 +0300

    Lab3(ada): add Producer, Consumer tasks seed code.

--- lab3/ada_lab/src/ada_lab.adb
+++ lab3/ada_lab/src/ada_lab.adb
@@ -1,6 +1,20 @@
 with Ada.Text_IO;
 
 procedure Ada_Lab is
+
+   task type Producer;
+   task type Consumer;
+
+   task body Producer is
+   begin
+      null;
+   end Producer;
+
+   task body Consumer is
+   begin
+      null;
+   end Consumer;
+
 begin
    Ada.Text_IO.Put_Line ("Init");
 end Ada_Lab;
