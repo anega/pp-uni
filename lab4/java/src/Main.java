f5e28d5c04244a7d39f4bb7b742c528e22d6c553
anegin <anegamega@gmail.com>
2023-07-12 21:19:15 +0300

    Lab4(java): use Philosophers and Forks in Main.

--- lab4/java/src/Main.java
+++ lab4/java/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    private static final int PHILOSOPHERS_QUANTITY = 5;
+    private static final Fork [] forks = new Fork[PHILOSOPHERS_QUANTITY];
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < PHILOSOPHERS_QUANTITY; i++) {
+            forks[i] = new Fork();
+        }
+
+        for (int i = 0; i < PHILOSOPHERS_QUANTITY; i++) {
+            Philosopher philosopher = new Philosopher(i, forks[i], forks[(i + 1) % PHILOSOPHERS_QUANTITY]);
+            philosopher.start();
+        }
     }
 }
