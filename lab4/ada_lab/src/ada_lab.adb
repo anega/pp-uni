f17f2bf83a7c890b82944ce37836a51effd497b9
anegin <anegamega@gmail.com>
2023-07-12 17:56:49 +0300

    Lab4(ada): add starter code.

--- lab4/ada_lab/src/ada_lab.adb
+++ lab4/ada_lab/src/ada_lab.adb
@@ -1,6 +1,47 @@
-with Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
 
-procedure Ada_Lab is
+with GNAT.Semaphores; use GNAT.Semaphores;
+
+procedure Dinner_Philosophers is
+   task type Phylosopher is
+      entry Start (Id : Integer);
+   end Phylosopher;
+
+   Forks : array (1 .. 5) of Counting_Semaphore (1, Default_Ceiling);
+
+   task body Phylosopher is
+      Id                          : Integer;
+      Id_Left_Fork, Id_Right_Fork : Integer;
+   begin
+      accept Start (Id : Integer) do
+         Phylosopher.Id := Id;
+      end Start;
+      Id_Left_Fork  := Id;
+      Id_Right_Fork := Id rem 5 + 1;
+
+      for I in 1 .. 10 loop
+         Put_Line ("Phylosopher " & Id'Img & " thinking " & I'Img & " time");
+
+         Forks (Id_Left_Fork).Seize;
+         Put_Line ("Phylosopher " & Id'Img & " took left fork");
+
+         Forks (Id_Right_Fork).Seize;
+         Put_Line ("Phylosopher " & Id'Img & " took right fork");
+
+         Put_Line ("Phylosopher " & Id'Img & " eating" & I'Img & " time");
+
+         Forks (Id_Right_Fork).Release;
+         Put_Line ("Phylosopher " & Id'Img & " put right fork");
+
+         Forks (Id_Left_Fork).Release;
+         Put_Line ("Phylosopher " & Id'Img & " put left fork");
+      end loop;
+   end Phylosopher;
+
+   Phylosophers : array (1 .. 5) of Phylosopher;
 begin
-   Ada.Text_IO.Put_line ("test");
-end Ada_Lab;
+   for I in Phylosophers'Range loop
+      Phylosophers (I).Start (I);
+   end loop;
+
+end Dinner_Philosophers;
