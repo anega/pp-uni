3d7369e9749664af8e0d4805e5f6cdb862e60e42
anegin <anegamega@gmail.com>
2023-07-13 21:30:46 +0300

    Lab4(c#): add Forks semaphores.

--- lab4/c_sharp/dining_philosophers/Program.cs
+++ lab4/c_sharp/dining_philosophers/Program.cs
@@ -4,26 +4,33 @@ public class DiningPhilosophers
 {
     private const int PhilosophersQuantity = 5;
     private static readonly Thread[] Philosophers = new Thread[PhilosophersQuantity];
+    private static readonly List<Semaphore> Forks = new(PhilosophersQuantity);
 
     private static void Main()
     {
         for (int i = 0; i < PhilosophersQuantity; i++)
         {
             int i1 = i;
-            Philosophers[i1] = new Thread(() => StartPhilosopher(i1));
+            Forks.Add(new Semaphore(1, 1));
+            Philosophers[i1] = new Thread(() =>
+                StartPhilosopher(i1, Forks[i1], Forks[(i1 + 1) % PhilosophersQuantity]));
             Philosophers[i1].Start();
         }
     }
 
-    private static void StartPhilosopher(int philosopherId)
+    private static void StartPhilosopher(int philosopherId, Semaphore leftFork, Semaphore rightFork)
     {
         while (true)
         {
             Console.WriteLine($"Philosopher {philosopherId} is thinking.");
+            leftFork.WaitOne();
             Console.WriteLine($"Philosopher {philosopherId} takes left fork.");
+            rightFork.WaitOne();
             Console.WriteLine($"Philosopher {philosopherId} takes right fork.");
             Console.WriteLine($"Philosopher {philosopherId} is eating.");
+            leftFork.Release();
             Console.WriteLine($"Philosopher {philosopherId} puts down left fork.");
+            rightFork.Release();
             Console.WriteLine($"Philosopher {philosopherId} puts down right fork.");
         }
     }
